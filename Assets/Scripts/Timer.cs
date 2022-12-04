7284d2c283c65d2d5f39c8c90cb2250c69ead9ab
Noah Kuzyk <coolxnoah@gmail.com>
2022-12-04 12:24:00 -0500

    Chpt 13: Debugging github

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text timerText;
+
+    void Awake()
+    {
+        //timerText = GetComponent<Text>();
+    }
+    void Update()
+    {
+        timerText.text = System.Math.Round((decimal)Time.timeSinceLevelLoad, 2).ToString();
+    }
+}
